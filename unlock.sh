e87767c183b66fd69bfb0d48fc6464037e29d31d
立音喵 <wqr3011@gmail.com>
2023-03-18 17:11:05 +0800

    Fix card check

--- unlock.sh
+++ unlock.sh
@@ -10,11 +10,6 @@ if ! [ -x "$(command -v ethtool)" ]; then
   exit 1
 fi
 
-if ! [ -x "$(command -v bc)" ]; then
-  echo 'Error: bc is not installed.' >&2
-  exit 1
-fi
-
 if ! [ -x "$(command -v awk)" ]; then
   echo 'Error: awk is not installed.' >&2
   exit 1
@@ -33,13 +28,13 @@ fi
 if [ -f "/sys/class/net/$intf/device/device" ];then
   vdr_id=$(cat /sys/class/net/$intf/device/vendor | xargs)
   dev_id=$(cat /sys/class/net/$intf/device/device | xargs)
-  if [[ "$vdr_id" == "0x8086" ]]; then
+  if ! [[ "$vdr_id" == "0x8086" ]]; then
      echo "Check failed: Interface $intf not Intel X520 Card (VDR $vdr_id)"
      exit 3
   fi
   if [[ "$dev_id" == "0x154d" || "$dev_id" == "0x10fb" ]]; then
     val=$(ethtool -e $intf offset 0x58 length 1 | tail -n 1 | cut -d : -f 2 | xargs)
-    val_bin=$(echo "obase=10; ibase=16; ${val^^}" | bc)
+    val_bin=$((16#$val))
     val_def=$((val_bin & 1))
     if [ $val_def -eq 1 ]; then
       echo "Card is unlocked."
@@ -47,7 +42,7 @@ if [ -f "/sys/class/net/$intf/device/device" ];then
     else
       echo "Unlocking..."
       new_val_dec=$((val_bin | 1))
-      new_val=$(echo "obase=16; ibase=10; ${new_val_dec^^}" | bc | awk '{print tolower($0)}')
+      new_val=$(printf '%x\n' $new_val_dec)
       magic="$dev_id8086"
       ethtool -E $intf magic $magic offset 0x58 value 0x$new_val length 1
       echo "Reboot the machine for changes to take effect..."
